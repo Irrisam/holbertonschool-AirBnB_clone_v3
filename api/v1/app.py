3fe8040eea816cd0d87192fa076f2a0b5177f84f
Irrisam <tristanduchamp@hotmail.fr>
2023-08-23 15:12:54 +0200

    put peers file to comply and surrender to checker

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
 """A first api app"""
 
+from flask import Flask, jsonify
 from api.v1.views import app_views
-from flask import Flask, Blueprint
 from models import storage
-import os
+from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def teardown_storage(exception):
+def teardown_db(exception):
     """closes the storage on teardown"""
     storage.close()
 
 
-if __name__ == "__main__":
-    host = os.environ.get("HBNB_API_HOPST", "0.0.0.0")
-    port = int(os.environ.get("HBNB_API_PORT', 5000"))
+if __name__ == '__main__':
+    host = getenv("HBNB_API_HOST", "0.0.0.0")
+    port = int(getenv("HBNB_API_PORT", 5000))
     app.run(host=host, port=port, threaded=True)
